be33a95f28e79d7eab22d4a528255dc58e49d71c
GUBBIB <moon.dragon250@gmail.com>
2025-11-13 23:42:35 +0900

    proxy 설정 확인용 header 수정

--- cuty_web/src/component/Header/Header.tsx
+++ cuty_web/src/component/Header/Header.tsx
@@ -1,8 +1,26 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
 
 const Header = () => {
+    const [ping, setPing] = useState("");
+
+    useEffect(() => {
+        const fetchTest = async () => {
+            try {
+                const res = await axios.get(`${__API_BASE__}/auth/ping`);
+                setPing(res.data);
+            } catch (err) {
+                console.log(err);
+            }
+        }
+        fetchTest();
+    }, []);
+
     return(
         <div id="Header">
             header test header
+            <span className='testPing'>{ping}</span>
         </div>
     );
 }
