d5f9ea99ff78695ea303d8ad7f2fee2b57b31234
GUBBIB <moon.dragon250@gmail.com>
2025-11-14 16:13:57 +0900

    sql 초기값 수정

--- cuty_app/temp_data.sql
+++ cuty_app/temp_data.sql
@@ -2,86 +2,107 @@
 
 -- Countries 테이블
 INSERT INTO countries (id, name, code) VALUES
-(1, '대한민국', 'KR'),
-(2, '미국', 'US'),
-(3, '일본', 'JP'),
-(4, '중국', 'CN'),
-(5, '독일', 'DE');
+(1, 'ADMIN', 'AD'),
+(2, '대한민국', 'KR'),
+(3, '미국', 'US'),
+(4, '일본', 'JP'),
+(5, '중국', 'CN'),
+(6, '독일', 'DE');
 
 -- Schools 테이블
 INSERT INTO schools (id, name, country_id) VALUES
+-- ADMIN
+(1, 'ADMIN', 1),
 -- 한국 학교들
-(1, '서울대학교', 1),
-(2, '연세대학교', 1),
-(3, '고려대학교', 1),
-(4, 'KAIST', 1),
+(2, '서울대학교', 2),
+(3, '연세대학교', 2),
+(4, '고려대학교', 2),
+(5, 'KAIST', 2),
 -- 미국 학교들
-(5, 'MIT', 2),
-(6, 'Stanford University', 2),
-(7, 'Harvard University', 2),
+(6, 'MIT', 3),
+(7, 'Stanford University', 3),
+(8, 'Harvard University', 3),
 -- 일본 학교들
-(8, '도쿄대학교', 3),
-(9, '와세다대학교', 3),
+(9, '도쿄대학교', 4),
+(10, '와세다대학교', 4),
 -- 중국 학교들
-(10, '베이징대학교', 4),
+(11, '베이징대학교', 5),
 -- 독일 학교들
-(11, '뮌헨공과대학교', 5);
+(12, '뮌헨공과대학교', 6);
 
 -- Colleges 테이블
 INSERT INTO colleges (id, name, school_id) VALUES
+(1, 'ADMIN', 1),
 -- 서울대학교
-(1, '공과대학', 1),
-(2, '경영대학', 1),
-(3, '인문대학', 1),
+(2, '공과대학', 2),
+(3, '경영대학', 2),
+(4, '인문대학', 2),
 -- 연세대학교
-(4, '공과대학', 2),
-(5, '경영대학', 2),
+(5, '공과대학', 3),
+(6, '경영대학', 3),
 -- 고려대학교
-(6, '공과대학', 3),
-(7, '경영대학', 3),
+(7, '공과대학', 4),
+(8, '경영대학', 4),
 -- KAIST
-(8, '전산학부', 4),
-(9, '공과대학', 4),
+(9, '전산학부', 5),
+(10, '공과대학', 5),
 -- MIT
-(10, 'School of Engineering', 5),
-(11, 'Sloan School of Management', 5),
+(11, 'School of Engineering', 6),
+(12, 'Sloan School of Management', 6),
 -- Stanford
-(12, 'School of Engineering', 6),
-(13, 'Graduate School of Business', 6);
+(13, 'School of Engineering', 7),
+(14, 'Graduate School of Business', 7);
 
 -- Departments 테이블
 INSERT INTO departments (id, name, college_id) VALUES
--- 서울대 공과대학
-(1, '컴퓨터공학부', 1),
-(2, '전자정보공학부', 1),
-(3, '기계공학부', 1),
--- 서울대 경영대학
-(4, '경영학과', 2),
--- 서울대 인문대학
-(5, '국어국문학과', 3),
-(6, '영어영문학과', 3),
--- 연세대 공과대학
-(7, '컴퓨터과학과', 4),
-(8, '전기전자공학과', 4),
--- 연세대 경영대학
-(9, '경영학과', 5),
--- 고려대 공과대학
-(10, '컴퓨터학과', 6),
-(11, '전기전자공학부', 6),
--- 고려대 경영대학
-(12, '경영학과', 7),
+(1, 'ADMIN', 1),
+
+-- 서울대 공과대학  
+(2, '컴퓨터공학부', 2),
+(3, '전자정보공학부', 2),
+(4, '기계공학부', 2),
+
+-- 서울대 경영대학 
+(5, '경영학과', 3),
+
+-- 서울대 인문대학 
+(6, '국어국문학과', 4),
+(7, '영어영문학과', 4),
+
+-- 연세대 공과대학 
+(8, '컴퓨터과학과', 5),
+(9, '전기전자공학과', 5),
+
+-- 연세대 경영대학 
+(10, '경영학과', 6),
+
+-- 고려대 공과대학 
+(11, '컴퓨터학과', 7),
+(12, '전기전자공학부', 7),
+
+-- 고려대 경영대학 
+(13, '경영학과', 8),
+
 -- KAIST
-(13, '전산학과', 8),
-(14, '기계공학과', 9),
-(15, '전기및전자공학부', 9),
+-- KAIST 전산학부 
+(14, '전산학과', 9),
+-- KAIST 공과대학 
+(15, '기계공학과', 10),
+(16, '전기및전자공학부', 10),
+
 -- MIT
-(16, 'Computer Science', 10),
-(17, 'Electrical Engineering', 10),
-(18, 'Management', 11),
+-- MIT School of Engineering 
+(17, 'Computer Science', 11),
+(18, 'Electrical Engineering', 11),
+-- MIT Sloan School of Management
+(19, 'Management', 12),
+
 -- Stanford
-(19, 'Computer Science', 12),
-(20, 'Electrical Engineering', 12),
-(21, 'MBA', 13);
+-- Stanford School of Engineering 
+(20, 'Computer Science', 13),
+(21, 'Electrical Engineering', 13),
+-- Stanford Graduate School of Business 
+(22, 'MBA', 14);
 
 -- nicknames 랜덤 값
 INSERT INTO nicknames (id, nickname, created_at, updated_at, deleted_at) VALUES
