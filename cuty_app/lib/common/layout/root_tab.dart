060087515c1c3bd0ef6cb05e6d2f7389cdb7aba6
GUBBIB <moon.dragon250@gmail.com>
2026-01-05 15:55:07 +0900

    Navigator Bar 생성

---
+++ cuty_app/lib/common/layout/root_tab.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+import '../../screens/home_screen.dart';
+
+class RootTab extends StatefulWidget{
+  const RootTab({super.key});
+
+  @override
+  State<RootTab> createState() => _RootTabState();
+}
+
+class _RootTabState extends State<RootTab> {
+  int index = 1;
+
+  List<Widget> screens = [
+    Center(child: Text("Shop Screen")), // ShopScreen() 으로 변경 필요
+    HomeScreen(),
+    Center(child: Text("My Screen"))    // MyScreen() 으로 변경 필요
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: screens[index],
+
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: index,
+        onTap: (int newIndex){
+          setState(() {
+            index = newIndex;
+          });
+        },
+        selectedItemColor: Colors.black,
+        unselectedItemColor: Colors.grey,
+        showSelectedLabels: true,
+        showUnselectedLabels: true,
+        type: BottomNavigationBarType.fixed,
+
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.store_outlined),
+            label: 'Shop',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person_outline),
+            label: 'My',
+          ),
+        ],
+      ),
+    );
+  }
+}
