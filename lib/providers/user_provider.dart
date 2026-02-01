41869b8dbb3a52baccd402372aedc05e03285934
backwalker <cutywalker@gmail.com>
2026-02-01 23:10:00 +0900

    Fix: Add missing visaType field to UserProvider to resolve build error

--- lib/providers/user_provider.dart
+++ lib/providers/user_provider.dart
@@ -4,22 +4,25 @@ class UserState {
   final String name;
   final String university;
   final String major;
+  final String visaType; // Added field
   
   UserState({
     this.name = '김쿠티',
     this.university = '한국대학교',
     this.major = '경영학과',
+    this.visaType = 'D-2', // Default value
   });
 }
 
 class UserNotifier extends StateNotifier<UserState> {
   UserNotifier() : super(UserState());
   
-  void updateUser({String? name, String? university, String? major}) {
+  void updateUser({String? name, String? university, String? major, String? visaType}) {
     state = UserState(
       name: name ?? state.name,
       university: university ?? state.university,
       major: major ?? state.major,
+      visaType: visaType ?? state.visaType,
     );
   }
 }
